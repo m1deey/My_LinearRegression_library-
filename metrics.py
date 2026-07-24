fb4769aeeff8fff637a5ce441421fcd012faf6cd
YASSER <299834908+m1deey@users.noreply.github.com>
2026-07-24 14:28:19 +0100

    Update metrics.py

--- metrics.py
+++ metrics.py
@@ -1,1 +1,10 @@
 
+import numpy as np
+
+def mse(y_true, y_pred):
+    return np.mean((y_true - y_pred) ** 2)
+
+def r2_score(y_true, y_pred):
+    ss_res = np.sum((y_true - y_pred) ** 2)
+    ss_tot = np.sum((y_true - np.mean(y_true)) ** 2)
+    return 1 - (ss_res / ss_tot)
